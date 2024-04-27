08d6eea4fc23e7f569bbfd883f0dc049272a4b47
Flavian Kaufmann <flavian@flaviankaufmann.ch>
2024-04-27 14:52:08 +0200

    added clock divider

--- src/top.v
+++ src/top.v
@@ -6,13 +6,21 @@ module top (
 
 reg [5:0] ctr_q;
 wire [5:0] ctr_d;
+wire clk_slow;
+assign reset = ~key;
 
-always @(posedge clk) begin
+clock_divider #(.N(10000000)) clk_div (
+  .clk(clk),
+  .clk_out(clk_slow),
+  .reset(reset)
+);
+
+always @(posedge clk_slow) begin
 	if (key) ctr_q <= ctr_d;
 	else ctr_q <= 6'b0;
 end
 
 assign ctr_d = ctr_q + 6'b1;
-assign led = ctr_q;
+assign led = ~ctr_q;
 
 endmodule
