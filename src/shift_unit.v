5d41601bb83859e684d28c6c6cdf093851722604
Flavian Kaufmann <flavian@flaviankaufmann.ch>
2024-05-01 16:57:36 +0200

    fixed unsigned not recognized in verilog 2000 bug

--- src/shift_unit.v
+++ src/shift_unit.v
@@ -2,7 +2,7 @@ module shift_unit #(
   parameter N = 32
 )(
   input signed [N-1:0] A,
-  input unsigned [N-1:0] SHAMT,
+  input  [N-1:0] SHAMT,
   input [1:0] OP, // 00: SLL, 01: SRL, 11: SRA 
   output reg [N-1:0] RESULT
 );
